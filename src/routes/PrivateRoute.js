7301e7ba4a8b8b697c7cf640891090a6cbce6c43
Matheus Rodrigues <matheusrf96@gmail.com>
2022-03-25 15:16:07 -0300

    Create private route

---
+++ src/routes/PrivateRoute.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function PrivateRoute({
+  component: Component,
+  isClosed,
+  ...rest
+}) {
+  const isLoggedIn = false;
+
+  if (isClosed && !isLoggedIn) {
+    return (
+      <Redirect
+        to={{ pathname: '/login', state: { prevPath: rest.location.pathname } }}
+      />
+    );
+  }
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Route {...rest} component={Component} />;
+}
+
+PrivateRoute.defaultProps = {
+  isClosed: false,
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+  isClosed: PropTypes.bool,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
+import PrivateRoute from './PrivateRoute';
 
 export default function AppRouter() {
   return (
     <Switch>
-      <Route exact path="/" component={Login} />
+      <PrivateRoute exact path="/" component={Login} isClosed />
       <Route path="*" component={Page404} />
     </Switch>
   );

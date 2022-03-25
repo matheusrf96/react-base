2ea10a13ca3233682cbbf5a21b895bac03e6bcfd
Matheus Rodrigues <matheusrf96@gmail.com>
2022-03-25 11:37:22 -0300

    Implement application colors

---
+++ src/config/colors.js
@@ -0,0 +1,7 @@
+export const primary = '#C307EF';
+export const primaryDark = '#1A1A1D';
+
+export const success = '#0197F6';
+export const info = '#0197F6';
+export const warning = '#F2AF29';
+export const error = '#F2AF29';
